9fe2f2d584f4efa12a0b6c2d9e92dd4d3588d1eb
Jonathan Lundholm <jon.lundholm@gmail.com>
2024-01-03 21:15:32 +0100

    feat(cli): aliases for install, uninstall and update

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
 use clap::{Args, Parser, Subcommand};
 
-/// ct -- manage your code tools with ease.
+/// dti -- manage your code tools with ease.
 #[derive(Parser)]
 #[command(author, version, about, long_about=None)]
 pub struct Cli {
@@ -15,17 +15,21 @@ pub enum Command {
     /// Checks for updates of packages.
     Check(CheckArgs),
     /// List installed and available packages.
+    #[command(visible_alias = "ls")]
     List(ListArgs),
     /// Install a package.
     /// Packages from ensure-installed will also be installed.
+    #[command(visible_alias = "i")]
     Install {
         name: Option<String>,
         /// Use as version to install. Else try to resolve latest version.
         version: Option<String>,
     },
     /// Uninstall a package.
+    #[command(visible_alias = "remove", visible_alias = "rm")]
     Uninstall { name: String },
     /// Updates a package.
+    #[command(visible_alias = "up")]
     Update {
         name: String,
         /// Version to update, or downgrade, to.
